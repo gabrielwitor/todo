304375ec9476c590e16fa9415238639587515fe8
Gabriel Witor <witor.alm@gmail.com>
2026-08-19 07:52:39 -0300

    feat: create TasksSummary Component

---
+++ src/core-components/task-summary.tsx
@@ -0,0 +1,24 @@
+import Badge from "../components/badge";
+import Text from "../components/text";
+
+function TasksSummary() {
+	return (
+		<>
+			<div className="flex items-center gap-2">
+				<Text variant={"body-sm-bold"} className="text-gray-300!">
+					Tarefas criadas
+				</Text>
+				<Badge variant={"secondary"}>5</Badge>
+			</div>
+
+			<div className="flex items-center gap-2">
+				<Text variant={"body-sm-bold"} className="text-gray-300!">
+					Concluídas
+				</Text>
+				<Badge>2 de 5</Badge>
+			</div>
+		</>
+	);
+}
+
+export default TasksSummary;
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,7 +1,14 @@
 import Container from "../components/container";
+import TasksSummary from "../core-components/task-summary";
 
 function PageHome() {
-	return <Container></Container>;
+	return (
+		<Container as="article">
+			<header className="flex justify-center">
+				<TasksSummary />
+			</header>
+		</Container>
+	);
 }
 
 export default PageHome;
