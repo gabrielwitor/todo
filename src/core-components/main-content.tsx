31464944a9bd4c309176d56c58d16833577fb4f9
Gabriel Witor <witor.alm@gmail.com>
2026-08-19 07:29:32 -0300

    feat: main and component routes, basic layout

---
+++ src/core-components/main-content.tsx
@@ -0,0 +1,14 @@
+import type React from "react";
+import { cx } from "class-variance-authority";
+
+type MainContentProps = React.ComponentProps<"main">;
+
+function MainContent({ children, className, ...props }: MainContentProps) {
+	return (
+		<main className={cx("mt-4 md:mt-8", className)} {...props}>
+			{children}
+		</main>
+	);
+}
+
+export default MainContent;
